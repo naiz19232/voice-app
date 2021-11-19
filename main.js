edda5e4c699a87b918e2611d9e9cb7fce485192d
naiz19232 <33213878+naiz19232@users.noreply.github.com>
2021-11-19 19:10:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,22 +10,7 @@ console.log(event);
 var Content = event.results[0][0].transcript;
 console.log(event);
 document.getElementById("textbox").innerHTML = Content;
-speak();
 }
-function speak(){
-var synth = window.speechSynthesis;
-speak_data = document.getElementById("textbox").value;
-var utterThis = new SpeechSynthesisUtterance(speak_data);
-synth.speak(utterThis);
-Webcam.attach(camera);
-}
-camera= document.getElementById("camera");
-Webcam.set({
-width:360,
-height:250,
-image_format : 'png',
-png_quality:90
-});
 
 
 
